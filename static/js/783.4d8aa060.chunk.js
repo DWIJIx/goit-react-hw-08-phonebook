9fd556f5512a75146b9622049066ae66a5c8be5f
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-04-30 17:23:45 +0000

    Deploying to gh-pages from @ DWIJIx/goit-react-hw-08-phonebook@9b28c16fd7fcd94b6025baa82b81347a443de6eb 🚀

--- static/js/783.4d8aa060.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[783],{2783:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var o,r=n(168),a=n(6444).ZP.h1(o||(o=(0,r.Z)(["\n  text-align: center;\n  color: darkblue;\n  font-size: 48px;\n  text-shadow: 7px -1px 5px rgba(0, 0, 0, 0.4);\n"]))),c=n(184);function u(){return(0,c.jsx)(a,{children:"Welcome to the phonebook!"})}}}]);
-//# sourceMappingURL=783.4d8aa060.chunk.js.map
---
+++ static/js/783.7dbdf1a7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[783],{2783:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var o,r=n(168),c=n(6444).ZP.h1(o||(o=(0,r.Z)(["\n  text-align: center;\n  color: grey;\n  font-size: 48px;\n"]))),u=n(184);function a(){return(0,u.jsx)(c,{children:"Welcome to the phonebook!"})}}}]);
+//# sourceMappingURL=783.7dbdf1a7.chunk.js.map
